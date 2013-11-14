9b4fe42d7863d19a5eb9f99f204d72a8a75193a8
Roberto C. Morano <rcmova@gmail.com>
2013-11-14 22:37:12 +0100

    added gecoscc-chef-server deploy script

---
+++ scripts/gecoscc-chef-server-install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Authors: Roberto C. Morano <rcmorano<at>emergya.com>
+#
+# Copyright 2013, Junta de Andalucia
+# http://www.juntadeandalucia.es/
+#
+# All rights reserved - EUPL License V 1.1
+# http://www.osor.eu/eupl
+
+export COOKBOOK_NAME='cookbook-gecoscc-chef-server'
+
+curl -L https://get.rvm.io | bash -s stable --ruby
+. /etc/profile.d/rvm.sh 
+rvm use ruby-2.0.0
+gem install chef --no-ri --no-rdoc
+
+cat > /tmp/solo.rb << EOF
+root = File.absolute_path(File.dirname(__FILE__))
+
+file_cache_path root
+cookbook_path root + '/cookbooks'
+EOF
+
+cat > /tmp/solo.json << EOF
+{
+    "run_list": [ "recipe[cookbook-gecoscc-chef-server::default]" ]
+}
+EOF
+
+mkdir /tmp/cookbooks
+wget -O /tmp/${COOKBOOK_NAME}.tar.gz https://api.github.com/repos/gecos-team/${COOKBOOK_NAME}/tarball 
+tar -C /tmp/cookbooks -zxf /tmp/${COOKBOOK_NAME}.tar.gz
+mv /tmp/cookbooks/*${COOKBOOK_NAME}* /tmp/cookbooks/${COOKBOOK_NAME}
+
+chef-solo -c /tmp/solo.rb -j /tmp/solo.json 
+echo "yes" | rvm implode
