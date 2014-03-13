1c40de5923b141333588e75145faa43320f6ebc0
Roberto C. Morano <rcmova@gmail.com>
2014-03-13 01:19:29 +0100

    added epel repo installation in rhel-based systems (needed for rvm)

--- scripts/gecoscc-chef-server-install.sh
+++ scripts/gecoscc-chef-server-install.sh
@@ -27,6 +27,7 @@ PLATFORM=$(ohai |grep platform_family|awk -F: '{print $2}'|sed 's|[", ]||g')
 
 case $PLATFORM in
   "rhel")
+    yum install -y http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
     yum install -y git
     ;;
   "debian")
