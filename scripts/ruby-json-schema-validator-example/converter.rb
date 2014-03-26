fc3ffdc1419c760109aed0c67b5146a4b3515f26
David Amian <damian@emergya.com>
2014-03-26 20:13:08 +0100

    Fix converter.rb

--- scripts/ruby-json-schema-validator-example/converter.rb
+++ scripts/ruby-json-schema-validator-example/converter.rb
@@ -1,7 +1,43 @@
 require 'json'
-recipe = ""
-resource = ""
+recipe = "users_mgmt"
+resource = "user_apps_autostart_res"
 resource_js = {
+  type: "object",
+  required: ["autostart_files"],
+  properties: {
+    autostart_files: {
+      type: "array",
+      minItems: 0,
+      uniqueItems: true,
+      items: {
+        type: "object",
+        required: ["user", "desktops"],
+        properties: {
+          user: {type: "string"},
+          desktops: {
+            type: "array",
+            minItems: 0,
+            uniqueItems: true,
+            items: {type: "string"}
+          }
+        }
+      }
+    },
+    job_ids: {
+        type: "array",
+        minItems: 0,
+        uniqueItems: true,
+        items: {
+          type: "object",
+          required: ["id"],
+          properties: {
+            id: { type: "string" },
+            status: { type: "string" }
+          }
+        }
+    }
+  }
+
 }
 
 complete_js = {
@@ -11,13 +47,18 @@ complete_js = {
   "type" => "object",
   "properties" => {
     "gecos_ws_mgmt" => {
+      
       "type" => "object",
+      "required" => [recipe],
+      "properties" => {
         recipe => {
           "type" => "object",
+          "required" => [resource],
           "properties" => {
             resource => resource_js
           }
         }
+      }
     }
   }
 }
